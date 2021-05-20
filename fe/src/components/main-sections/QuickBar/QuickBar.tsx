7fbc4c1904ba17936ee97b8d0cad45aea369918d
buugman195 <bugman195@gmail.com>
2021-05-20 23:55:08 +0800

    Setup Redux dev tool

--- fe/src/components/main-sections/QuickBar/QuickBar.tsx
+++ fe/src/components/main-sections/QuickBar/QuickBar.tsx
@@ -1,14 +1,10 @@
 import React from 'react'
 
-import { useStoreState } from '../../../store'
 import QuickBtnSet from './QuickBtnSet'
 
 import styles from './styles.module.scss'
 
 const QuickBar = () => {
-  // FIXME: Remove next 2 lines after Redux dev tool is configured
-  const state = useStoreState((state) => state)
-  console.log(state)
   return (
     <div className={styles.container}>
       <div className={styles.quick_buttons}>
--- fe/src/store/index.ts
+++ fe/src/store/index.ts
@@ -1,10 +1,18 @@
-import { createStore } from 'redux'
+import { createStore, compose, applyMiddleware } from 'redux'
 import { useSelector, TypedUseSelectorHook } from 'react-redux'
 
 import { StoreState } from './interfaces'
 import rootReducer from './rootReducer'
 
-const store = createStore(rootReducer)
+const composeEnhancers =
+  (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
+const middleWares: any[] = []
+
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(...middleWares))
+)
 
 const useStoreState: TypedUseSelectorHook<StoreState> = useSelector
 
