e10ee880a6f6f9c8e0670c580cd3a471e011d9b1
buugman195 <bugman195@gmail.com>
2021-07-02 00:03:20 +0800

    Add divider

---
+++ fe/src/components/ui/Divider/Divider.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import styles from './styles.module.scss'
+
+const Divider = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.divider} />
+    </div>
+  )
+}
+
+export default Divider
